baa325077308ec113da9b49dcc2de0cc56f26d56
terabytesoftw <terabytesoftw@gmail.com>
2020-06-08 18:47:57 -0400

    Update packages.

--- es/assets/js/just-the-docs.js
+++ es/assets/js/just-the-docs.js
@@ -44,7 +44,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://localhost:4000/es/assets/js/search-data.json', true);
+  request.open('GET', 'https://yii-extension.github.io/blog.github.io/es/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
